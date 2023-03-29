982dc502a4180f3ec9e10a9419817bfb01f20876
DylanTHK <dylantanhk@gmail.com>
2023-03-29 20:50:26 +0800

    initial commit

---
+++ server-marvel/src/test/java/com/workshop39/servermarvel/ServerMarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop39.servermarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
